875bdf97de49c070b56801a0813853289bf92649
mac <mac@11street.co.th>
2018-02-20 08:53:41 +0700

    Add findByTitle and findByDueDate API

--- src/main/java/com/mackittipat/mactodorest/jpa/repo/TodoRepo.java
+++ src/main/java/com/mackittipat/mactodorest/jpa/repo/TodoRepo.java
@@ -2,9 +2,17 @@ package com.mackittipat.mactodorest.jpa.repo;
 
 import com.mackittipat.mactodorest.jpa.entity.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @RepositoryRestResource(path = "todos")
 public interface TodoRepo extends CrudRepository<Todo, Long> {
 
+    Iterable<Todo> findByTitle(@Param("title") String title);
+
+    Iterable<Todo> findByDueDate(@Param("dueDate")
+                                 @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm") LocalDateTime dueDate);
 }
