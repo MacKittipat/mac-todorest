c2dbdb4c434e3af58f2d64de19d4a1fec9729cae
mac <mac@11street.co.th>
2018-02-20 15:23:16 +0700

    Finish unit test

--- src/test/java/com/mackittipat/mactodorest/jpa/repo/TodoRepoTest.java
+++ src/test/java/com/mackittipat/mactodorest/jpa/repo/TodoRepoTest.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mackittipat.mactodorest.jpa.entity.Todo;
-import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(secure = false)
 public class TodoRepoTest {
 
     @Autowired
@@ -37,7 +37,9 @@ public class TodoRepoTest {
     @Autowired
     private TodoRepo todoRepo;
 
-    @After
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    @Before
     public void clear(){
         todoRepo.deleteAll();
     }
@@ -45,88 +47,94 @@ public class TodoRepoTest {
     @Test
     public void testInsert() throws Exception {
 
-        String title = "Title1";
-
         Todo todo = new Todo();
-        todo.setTitle(title);
+        todo.setTitle("Title1");
         todo.setDesc("Desc1");
         todo.setDueDate(LocalDateTime.now());
+
+        List<Todo> todoResultList = (List<Todo>) todoRepo.findAll();
+        Assert.assertEquals(0, todoResultList.size());
+
         mockMvc.perform(post("/todos").content(toJsonString(todo)))
                 .andExpect(status().isCreated());
 
-        mockMvc.perform(get("/todos/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title", is(title)));
+        todoResultList = (List<Todo>) todoRepo.findAll();
+        Assert.assertEquals(1, todoResultList.size());
     }
 
     @Test
     public void testUpdate() throws Exception {
 
-        String title = "Title1";
-        String title2 = "Title2";
-
         Todo todo = new Todo();
-        todo.setTitle(title);
+        todo.setTitle("Title1");
         todo.setDesc("Desc1");
         todo.setDueDate(LocalDateTime.now());
-        mockMvc.perform(post("/todos").content(toJsonString(todo)))
-                .andExpect(status().isCreated());
 
-        todo.setTitle(title2);
-        mockMvc.perform(put("/todos/1").content(toJsonString(todo)));
+        todo = todoRepo.save(todo);
 
-        mockMvc.perform(get("/todos/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title", is(title2)));
+        todo.setTitle("Title2");
+        mockMvc.perform(put("/todos/" + todo.getId()).content(toJsonString(todo)));
 
+        Todo todoResult = todoRepo.findOne(todo.getId());
+        Assert.assertEquals(todo.getTitle(), todoResult.getTitle());
     }
 
     @Test
     public void testDelete() throws Exception {
-        Todo todo = new Todo();
-        todo.setTitle("Test Title");
-        todo.setDesc("Test Desc");
-        todo.setDueDate(LocalDateTime.now());
-        mockMvc.perform(post("/todos").content(toJsonString(todo)))
-                .andExpect(status().isCreated());
 
-        mockMvc.perform(delete("/todos/1"))
+        Todo todo = new Todo();
+        todo.setTitle("Title1");
+        todo.setDesc("Desc1");
+        todo.setDueDate(LocalDateTime.now());
+
+        todo = todoRepo.save(todo);
+
+        Todo todoResult = todoRepo.findOne(todo.getId());
+        Assert.assertEquals(todo.getId(), todoResult.getId());
+
+        mockMvc.perform(delete("/todos/" + todo.getId()))
                 .andExpect(status().isNoContent());
+
+        todoResult = todoRepo.findOne(todo.getId());
+        Assert.assertNull(todoResult);
     }
 
     @Test
     public void testFindByTitle() throws Exception {
-        String title = "Title1";
 
         Todo todo = new Todo();
-        todo.setTitle(title);
+        todo.setTitle("Title1");
         todo.setDesc("Desc1");
         todo.setDueDate(LocalDateTime.now());
-        mockMvc.perform(post("/todos").content(toJsonString(todo)))
-                .andExpect(status().isCreated());
 
-        mockMvc.perform(get("http://localhost:8080/todos/search/findByTitle?title=" + title))
+        todo = todoRepo.save(todo);
+
+        mockMvc.perform(get("/todos/search/findByTitle?title=" + todo.getTitle()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$._embedded.todos[0].title", is(title)));
+                .andExpect(jsonPath("$._embedded.todos[0].title", is(todo.getTitle())))
+                .andExpect(jsonPath("$._embedded.todos[0].desc", is(todo.getDesc())))
+                .andExpect(jsonPath("$._embedded.todos[0].dueDate", is(todo.getDueDate().format(formatter))))
+        ;
     }
 
     @Test
     public void testFindByDueDate() throws Exception {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
         LocalDateTime now = LocalDateTime.now();
-        String title = "Title1";
 
         Todo todo = new Todo();
-        todo.setTitle(title);
+        todo.setTitle("Title1");
         todo.setDesc("Desc1");
-        todo.setDueDate(now);
-        mockMvc.perform(post("/todos").content(toJsonString(todo)))
-                .andExpect(status().isCreated());
+        todo.setDueDate(LocalDateTime.parse(now.format(formatter), formatter));
 
-        mockMvc.perform(get("http://localhost:8080/todos/search/findByDueDate?dueDate=" + now.format(formatter)))
+        todo = todoRepo.save(todo);
+
+        mockMvc.perform(get("/todos/search/findByDueDate?dueDate=" + now.format(formatter)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$._embedded.todos[0].title", is(title)));
+                .andExpect(jsonPath("$._embedded.todos[0].title", is(todo.getTitle())))
+                .andExpect(jsonPath("$._embedded.todos[0].desc", is(todo.getDesc())))
+                .andExpect(jsonPath("$._embedded.todos[0].dueDate", is(todo.getDueDate().format(formatter))))
+        ;
     }
 
     private static String toJsonString(Todo todo) {
